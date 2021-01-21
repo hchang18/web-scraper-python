f85efaa293ea2fc8c7caf1868691a460bf412b99
Haeyoon Chang <haeyoon@pdx.edu>
2021-01-20 17:41:05 -0800

    fixed a typo

--- stackoverflow.py
+++ stackoverflow.py
@@ -35,7 +35,7 @@ def extract_job(html):
         'title': title,
         'company': company,
         'location': location,
-        'link': f"https://stackoverflow.com/jobs{job_id}"
+        'link': f"https://stackoverflow.com/jobs/{job_id}"
     }
 
 
