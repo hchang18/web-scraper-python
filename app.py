299dcdf3821f01ee4942f52bceb9158671bde46a
Haeyoon Chang <haeyoon@pdx.edu>
2021-01-23 17:13:46 -0800

    deactivate threading

--- app.py
+++ app.py
@@ -72,4 +72,4 @@ def export():
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for
     # multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(port=5000)
